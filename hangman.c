937f6c2fa5cb5da9253b62f782a0b609cfc7eec8
Scarcy <monsoyen@gmail.com>
2023-08-07 21:08:44 +0000

    I now read the words from a CSV file instead of manually creating words in the code

--- hangman.c
+++ hangman.c
@@ -10,6 +10,8 @@
 #define randomInt(n) (rand() % (n))
 #endif
 
+#define MAX_LINE_SIZE 1024
+#define MAX_WORD_SIZE 20
 char *initBoard(char *puzzleState, char *randomWord) {
   printf("Welcome to Hangman!\n");
   printf("Guess a letter\n");
@@ -22,6 +24,44 @@ char *initBoard(char *puzzleState, char *randomWord) {
   return puzzleState;
 }
 
+char **readFromFile(int *word_count) {
+  FILE *csvFile = fopen("res/words.csv", "r");
+  char **words = NULL;
+
+  if (!csvFile) {
+    perror("Error opening file");
+    return NULL;
+  }
+
+  char lineBuffer[MAX_LINE_SIZE]; // Buffer that holds every Line of characters
+  char *token; // Buffer that holds each String, split by commas
+
+  while (fgets(lineBuffer, sizeof(lineBuffer), csvFile)) {
+    token = strtok(lineBuffer, ",");
+    while (token) {
+      *word_count += 1;
+
+      words =
+          realloc(words, (*word_count) *
+                             sizeof(char *)); // Allocates more memory to hold
+                                              // pointers in the words array
+      words[*word_count - 1] = malloc(
+          strlen(token) + 1); // Allocates memory for the word itself. The +1 is
+                              // for the end of string character. ("\0")
+      strcpy(words[*word_count - 1], token);
+
+      token = strtok(NULL, ",");
+    }
+  }
+  fclose(csvFile);
+  return words;
+}
+void freeWordAllocation(char **words, int count) {
+  for (int i = 0; i < count; i++) {
+    free(words[i]);
+  }
+  free(words);
+}
 char *getRandomWord(char **words, int wordCount) {
 
   int randomIndex = randomInt(wordCount);
@@ -59,21 +99,14 @@ void finishGame(bool result, char *word) {
   }
 }
 int main() {
-  char *words[7];
+  int wordCount = 0;
+  char **wordsFromFile = readFromFile(&wordCount);
   char guessedChar;
   int failCount = 0;
-  int *correctCount = malloc(sizeof(int));
-  *correctCount = 0;
+  int correctCount = 0;
   bool gameCompleted = false;
-  words[0] = "programmering";
-  words[1] = "monitor";
-  words[2] = "minecraft";
-  words[3] = "strikking";
-  words[4] = "langrenn";
-  words[5] = "mikrofon";
-  words[6] = "tastatur";
   srand(time(NULL));
-  char *randomWord = getRandomWord(words, 7);
+  char *randomWord = getRandomWord(wordsFromFile, wordCount);
   char *puzzleState = malloc((strlen(randomWord) + 1) * sizeof(char));
   if (!puzzleState) {
     printf("Memory Allocation failed");
@@ -85,25 +118,26 @@ int main() {
 
   while (failCount < 7 && !gameCompleted) {
     printf("Fail Count: %d of 7\n", failCount);
-    printf("Correct: %d of %lu\n", *correctCount, strlen(randomWord));
+    printf("Correct: %d of %lu\n", correctCount, strlen(randomWord));
     printf("=> %s\n", puzzleState);
     printf("Guess: ");
     scanf(" %c", &guessedChar);
 
     bool checkanswer =
-        checkAnswer(guessedChar, randomWord, puzzleState, correctCount);
+        checkAnswer(guessedChar, randomWord, puzzleState, &correctCount);
     if (checkanswer != true) {
       failCount++;
       if (failCount == 7) {
         finishGame(false, randomWord);
       }
-    } else if (*correctCount == strlen(randomWord)) {
+    } else if (correctCount == strlen(randomWord)) {
       gameCompleted = true;
       printf("=> %s\n", puzzleState);
       finishGame(true, randomWord);
     }
   }
   free(puzzleState);
-  free(correctCount);
+  freeWordAllocation(wordsFromFile, wordCount);
+
   return 0;
 }
